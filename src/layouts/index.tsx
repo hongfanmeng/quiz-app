865d1fa30555532c3ffa5a72a729b18842c2c5f9
Steven Hong <hongfanmeng@gmail.com>
2024-04-07 22:38:10 +0800

    style: fix navbar layout

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -18,8 +18,8 @@ export const RootLayout: React.FC<LayoutProps> = ({ children }) => {
 
   return (
     <main className="h-screen w-full flex flex-col">
-      <div className="navbar bg-primary px-6 items-center">
-        <div className="container max-w-[1280px] mx-auto">
+      <div className="bg-primary">
+        <div className="container max-w-[1280px] mx-auto flex px-6 py-2 items-center">
           <img alt="icon" src="/icon.png" className="w-8 h-8" />
           <a className="font-bold text-2xl text-white ml-4">複習問答</a>
           <div className="flex-1" />
